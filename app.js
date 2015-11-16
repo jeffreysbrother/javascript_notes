dc472b58d0378165e487e735f0b26a3e839fb831
James Cool <jeffreysbrother@gmail.com>
2015-11-16 14:21:55 -0800

    constructor functions

--- app.js
+++ app.js
@@ -24,3 +24,33 @@ var jane = {
 
 jane.__proto__ = person;
 console.log(jane.getFullName());
+
+for (var prop in john) {
+  if (john.hasOwnProperty(prop)) {
+    console.log(prop + ": " + john[prop]);
+  }
+}
+
+
+//--------------------------------------------------------//
+
+
+//Everything is an object (or a primitive)
+
+var a = {};
+var b = function(){};
+var c = [];
+
+
+//--------------------------------------------------------//
+//other ways to build objects and set prototypes
+
+//function constructors and the "new" keyword
+
+function Person() {
+  this.firstname = 'John';
+  this.lastname = 'Doe';
+}
+
+var john = new Person();
+console.log(john);
