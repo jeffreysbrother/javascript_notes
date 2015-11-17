bdbf82879eb74df2560d87146a7f69dfafb1a906
James Cool <jeffreysbrother@gmail.com>
2015-11-16 18:37:46 -0800

    pure prototypal inheritance with Object dot create

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 //prototypal inheritance in JavaScript
 
-var person = {
-  firstname: 'Default',
-  lastname: 'Default',
-  getFullName: function(){
-    return this.firstname + ' ' + this.lastname;
-  }
-};
-
-var john = {
-  firstname: 'John',
-  lastname: 'Doe'
-};
+// var person = {
+//   firstname: 'Default',
+//   lastname: 'Default',
+//   getFullName: function(){
+//     return this.firstname + ' ' + this.lastname;
+//   }
+// };
+//
+// var john = {
+//   firstname: 'John',
+//   lastname: 'Doe'
+// };
 
 //don't ever do this: __proto__ is deprecated
-john.__proto__ = person;
-console.log(john.getFullName());
-console.log(john.firstname);
-
-var jane = {
-  firstname: 'Jane'
-};
-
-jane.__proto__ = person;
-console.log(jane.getFullName());
-
-for (var prop in john) {
-  if (john.hasOwnProperty(prop)) {
-    console.log(prop + ": " + john[prop]);
-  }
-}
+// john.__proto__ = person;
+// console.log(john.getFullName());
+// console.log(john.firstname);
+//
+// var jane = {
+//   firstname: 'Jane'
+// };
+//
+// jane.__proto__ = person;
+// console.log(jane.getFullName());
+//
+// for (var prop in john) {
+//   if (john.hasOwnProperty(prop)) {
+//     console.log(prop + ": " + john[prop]);
+//   }
+// }
 
 
 //--------------------------------------------------------//
@@ -37,9 +37,9 @@ for (var prop in john) {
 
 //Everything is an object (or a primitive)
 
-var a = {};
-var b = function(){};
-var c = [];
+// var a = {};
+// var b = function(){};
+// var c = [];
 
 
 //--------------------------------------------------------//
@@ -47,13 +47,18 @@ var c = [];
 
 //function constructors and the "new" keyword
 
-function Person() {
-  this.firstname = 'John';
-  this.lastname = 'Doe';
-}
 
-var john = new Person();
-console.log(john);
+
+// function Person() {
+//   this.firstname = 'John';
+//   this.lastname = 'Doe';
+// }
+//
+// var john = new Person();
+// console.log(john);
+
+
+
 
 //constructor functions were only implemented to try to appease programmers coming from other languages
 //make sure you're using them properly
@@ -61,3 +66,26 @@ console.log(john);
 //2. must also use the new keyword
 //you can use a linter to warn you about these mistakes
 //constructor functions are likely going to be superceded in the future
+
+
+
+//--------------------------------------------------------//
+
+//other languages have classes
+//embrace that JavaScript has prototypal inheritance
+//Object.create : pure prototypal inheritance
+
+var person = {
+  firstname: 'default',
+  lastname: 'default',
+  greet: function() {
+    console.log('Hi ' + this.firstname);
+  }
+};
+
+var lacey = Object.create(person);
+lacey.firstname = 'Lacey';
+lacey.lastname = 'Junior';
+// console.log(lacey);
+
+lacey.greet();
